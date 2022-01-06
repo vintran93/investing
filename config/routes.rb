df18782f1519b8962c0e24b0ffaea06abacfe048
Vincent Tran <vincentuong@yahoo.com>
2022-01-05 22:08:17 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,31 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'home#index'
+  get 'home/about'
+  get 'home/search'
+  get 'currencies/table'
+
+  get 'currencies/new', to: 'currencies#new', as: 'new_currency'
+  get 'currencies', to: 'currencies#index', as: 'currencies'
+  get 'currencies/:id/edit', to: 'currencies#edit', as: 'edit_currency'
+  get 'currencies/:id', to: 'currencies#show', as: 'currency'
+  post 'currencies', to: 'currencies#create' 
+  
+  patch 'currencies/:id', to: 'currencies#update'
+  put 'currencies/:id', to: 'currencies#update'
+  delete '/currencies/:id', to: 'currencies#destroy'
+
+  post '/home/search' => 'home/search'
+
+  get 'portfolios/new', to: 'portfolios#new', as: 'new_portfolio'
+  get 'portfolios', to: 'portfolios#index', as: 'portfolios'
+  get 'portfolios/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
+  get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio'
+
+  patch 'portfolios/:id', to: 'portfolios#update'
+  put 'portfolios/:id', to: 'portfolios#update'
+  delete '/portfolios/:id', to: 'portfolios#destroy'
+
+  post '/portfolios', to: 'portfolios#create'
 end
